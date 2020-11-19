aa5bfa9464f3c03e166d0b92e420a0e245bcb79a
Shusei-Dev <matissem33@gmail.com>
2020-11-19 19:30:29 +0100

    Deletes all file

--- src/settings.py
+++
@@ -1,7 +0,0 @@
-
-
-class settings:
-    fps = 60 #<-- max fps
-    full_screen = True #<-- FullScreen option
-    version = "0.0.1" #<-- Version of the game
-    name = "PyMoba" #<-- Name of the game
